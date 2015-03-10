1468ac64d98bdac91f84b92597a889be41dee4b9
MUCb <vitalik560@yandex.ru>
2015-03-10 15:03:20 -0700

    add check password length

--- main.c
+++ main.c
@@ -12,7 +12,7 @@
 
 #define __BUFLEN__ 1024
 #define __USER_NAME_LEN__ 34
-#define __PASSWORD_LEN__ 34
+#define __PASSWORD_LEN__ 256
 
 int main(int argc, char * argv[])
 {
@@ -43,45 +43,84 @@ int main(int argc, char * argv[])
 					{
 						 fprintf(stdout, "Client connected: %s\n",
 											inet_ntoa(cli_addr.sin_addr));
+						struct timeval tv;
+
+						tv.tv_sec = 10;  /* 30 Secs Timeout */
+						tv.tv_usec = 0;  // Not init'ing this can cause strange errors
+
+						setsockopt(cl_socket, SOL_SOCKET, SO_RCVTIMEO, (char *)&tv,sizeof(struct timeval));
+
 						if(send(cl_socket, "login:", strlen("login:")+1, 0) > 0)
 						{
+							int recv_size = 0;
 							bzero(buffer, __BUFLEN__);
-							int recv_size = recv(cl_socket, buffer, __USER_NAME_LEN__, 0);
-							if(recv_size > 0)
+							int recv_flag = 0;
+							do
 							{
-								fprintf(stdout, "resv %d bytes |%s|\n", recv_size, buffer );
-								if((buffer[recv_size-1] == '\n') && (buffer[recv_size-2] == '\r'))
+								int t = recv(cl_socket, buffer, __USER_NAME_LEN__, 0);
+								if(t > 0)
 								{
-									// printf("Success\n");
-									if(send(cl_socket, "password:", strlen("password:")+1, 0) > 0)
+									recv_size += t;
+									fprintf(stdout, "resv %d bytes |%s|\n", recv_size, buffer );
+									if((buffer[recv_size-1] == '\n') && (buffer[recv_size-2] == '\r'))
 									{
-										bzero(buffer, __BUFLEN__);
-										int recv_size = recv(cl_socket, buffer, __USER_NAME_LEN__, 0);
-										if(recv_size > 0)
-										{
+										
+										recv_flag = 1;
+										break;
+									}
+								}
+								else
+								{
+									error_at_line(0, errno, __FILE__, __LINE__, "recv()");
+								}
 
+							}
+							while(recv_size < __PASSWORD_LEN__);
+							if(!recv_flag) 
+							{
+								const char * err_user = "ERROR user name is to long\n";
+								if(send(cl_socket, err_user, strlen(err_user)+1, 0) <= 0)
+									error_at_line(0, errno, __FILE__, __LINE__, "send()");
+								error_at_line(0, errno, __FILE__, __LINE__, "user name to long");
+							}
+							else
+							{
+								if(send(cl_socket, "password:", strlen("password:")+1, 0) > 0)
+								{
+									int recv_size = 0;
+									bzero(buffer, __BUFLEN__);
+									int recv_flag = 0;
+									do
+									{
+										int t = recv(cl_socket, buffer + recv_size, __PASSWORD_LEN__ - recv_size, 0);
+										if(t > 0)
+										{
+											recv_size += t;
+											fprintf(stdout, "resv %d bytes |%s|\n", recv_size, buffer );
+											if((buffer[recv_size-1] == '\n') && (buffer[recv_size-2] == '\r'))
+											{
+												recv_flag = 1;
+												break;
+											}
 										}
 										else
 										{
 											error_at_line(0, errno, __FILE__, __LINE__, "recv()");
 										}
 									}
-									else
+									while(recv_size < __PASSWORD_LEN__);
+									if(!recv_flag) 
 									{
+										const char * err_user = "ERROR password is to long\n";
+										if(send(cl_socket, err_user, strlen(err_user)+1, 0) <= 0)
 											error_at_line(0, errno, __FILE__, __LINE__, "send()");
+										error_at_line(0, errno, __FILE__, __LINE__, "password to long");
 									}
 								}
 								else
-								{ 
-									const char * err_user = "ERROR user name is to long\n";
-									if(send(cl_socket, err_user, strlen(err_user)+1, 0) <= 0)
-										error_at_line(0, errno, __FILE__, __LINE__, "send()");
-									error_at_line(0, errno, __FILE__, __LINE__, "user name to long");
+								{
+									error_at_line(0, errno, __FILE__, __LINE__, "send()");
 								}
-							}
-							else
-							{
-								error_at_line(0, errno, __FILE__, __LINE__, "recv()");
 							}
 						}
 						else
